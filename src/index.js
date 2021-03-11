d1c455e59ecefb475f7b4484ddaf08ef8e21c438
Irina-coud <irinatobolich313@gmail.com>
2021-03-11 23:41:05 +0300

    task solved

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.min(...array);
 }
 
-exports.max = function max (array) {
-  return 0;
+
+
+
+exports.max = function max(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  if (array === undefined || array.length === 0) {
+    return 0;
+  }
+  let avg = array.reduce(function (prev, item) {
+    return prev + item;
+  }, 0);
+  return avg / array.length;
 }

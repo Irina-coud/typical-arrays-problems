ccf33270e5924124fe2bef9f9044469f9bf08b5c
Irina-coud <irinatobolich313@gmail.com>
2021-07-07 10:59:09 +0300

    add new solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,63 @@
 
+// exports.min = function min(array) {
+//   if (array === undefined || array.length === 0) {
+//     return 0;
+//   }
+//   return Math.min(...array);
+// }
+
+// exports.max = function max(array) {
+//   if (array === undefined || array.length === 0) {
+//     return 0;
+//   }
+//   return Math.max(...array);
+// }
+
+// exports.avg = function avg(array) {
+//   if (array === undefined || array.length === 0) {
+//     return 0;
+//   }
+//   let avg = array.reduce(function (prev, item) {
+//     return prev + item;
+//   }, 0);
+//   return avg / array.length;
+// }
+
 exports.min = function min(array) {
   if (array === undefined || array.length === 0) {
     return 0;
   }
-  return Math.min(...array);
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (min > array[i]) {
+      min = array[i];
+    }
+  }
+  return min;
 }
-
-
-
 
 exports.max = function max(array) {
   if (array === undefined || array.length === 0) {
     return 0;
   }
-  return Math.max(...array);
+  let max = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (max < array[i]) {
+      max = array[i];
+    }
+  }
+  return max;
 }
 
 exports.avg = function avg(array) {
   if (array === undefined || array.length === 0) {
     return 0;
   }
-  let avg = array.reduce(function (prev, item) {
-    return prev + item;
-  }, 0);
-  return avg / array.length;
+  let med = 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  med = sum / array.length
+  return med;
 }
